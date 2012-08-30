2c87f49ba6a2442378006b8b7fc4f87cf9528a0f
Ben Straub <ben@straub.cc>
2012-08-30 13:22:01 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wacomxs::Application.config.secret_token = '726c05f698c8872298182bb8bba2fc03e212e59d56ee0c361096ef21d2ab93626c1e5e4f6d5972662efccf28e6e3b6793bf112b0cbb342a6b1de133fe0aedd31'
