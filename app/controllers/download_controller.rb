96e320bab3f54be187d58ad25f2450d9bd1ceda0
Ben Straub <ben@straub.cc>
2013-01-10 19:27:49 -0800

    Patch rails vulnerability (-> 3.2.11)

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -2,7 +2,8 @@ class DownloadController < ApplicationController
 	def main
 		@reco = Recommendation.find_by_id params[:id]
 		filename = @reco.title.to_ascii.parameterize
-		content = render_to_string :template => "download/main.xml.builder", :layout => false
+		content = render_to_string :template => "download/main",
+			:formats => [:xml], :layout => false
 		send_data content,
 			:type => 'text/xml',
 			:disposition => "attachment; filename=#{filename}.wacomxs"
@@ -10,6 +11,7 @@ class DownloadController < ApplicationController
 
 	def raw
 		@reco = Recommendation.find_by_id params[:id]
-		render :template => 'download/main.xml.builder', :layout => false
+		render :template => 'download/main',
+			:formats => [:xml], :layout => false
 	end
 end
